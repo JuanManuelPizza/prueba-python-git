87d312a64075dc93307d3b1967c6cc239c817289
Juan Pizza <juanchoep38@gmail.com>
2022-12-10 21:28:46 -0500

    ejercicio basico para prueba 2

---
+++ ejercicio-2-1.py
@@ -0,0 +1,6 @@
+#no se puede hacer uso de bucles y otros.
+
+nombre = input("Por favor ingrese su primer nombre: ")
+cantidad = int(input("Por favor ingrese el número de veces que quiere que este se muestre: "))
+
+print((nombre + "\n")* cantidad)
